241af619d07b429e78bbbeca8fc4a3c7432d6c70
Kairat Baigazakov <71325900+Kairat-Baigazakov@users.noreply.github.com>
2024-09-18 12:07:29 +0500

    Update forms.py
    Rechange form fields

--- app/forms.py
+++ app/forms.py
@@ -6,4 +6,4 @@ class CoinForm(forms.ModelForm):
 
     class Meta:
         model = CoinObj
-        fields = ('title.title', 'title.description', 'type.title', 'theme.title', 'nominal', 'diameter', 'weight')
+        fields = ('title', 'type', 'theme', 'nominal', 'diameter', 'weight')
