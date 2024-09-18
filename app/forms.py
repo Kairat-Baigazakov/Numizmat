125ac6aec2f8ad1de6165e203a290e621c32a7b2
Kairat <71325900+Kairat-Baigazakov@users.noreply.github.com>
2024-09-18 13:38:04 +0500

    Update forms.py
    Back to first commit

--- app/forms.py
+++ app/forms.py
@@ -4,15 +4,15 @@ from .models import CoinObj
 
 class CoinForm(forms.ModelForm):
 
-    title = forms.CharField(max_length=60)
-    description = forms.CharField(max_length=100)
-    type = forms.CharField(max_length=30)
-    proba = forms.IntegerField()
-    technology = forms.CharField(max_length=50)
-    theme = forms.CharField(max_length=30)
-    nominal = forms.IntegerField()
-    weight = forms.IntegerField()
-    diameter = forms.IntegerField()
-    #class Meta:
-    #    model = CoinObj
-    #    fields = ('title', 'type', 'theme', 'nominal', 'diameter', 'weight')
+    # title = forms.CharField(max_length=60)
+    # description = forms.CharField(max_length=100)
+    # type = forms.CharField(max_length=30)
+    # proba = forms.IntegerField()
+    # technology = forms.CharField(max_length=50)
+    # theme = forms.CharField(max_length=30)
+    # nominal = forms.IntegerField()
+    # weight = forms.IntegerField()
+    # diameter = forms.IntegerField()
+    class Meta:
+       model = CoinObj
+       fields = ('title', 'type', 'theme', 'nominal', 'diameter', 'weight')
